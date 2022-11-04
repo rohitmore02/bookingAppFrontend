739c58b1ebc74db7c9c07fae8da2c4942788f169
rohitmore02 <rohitmore386@gmail.com>
2022-11-04 14:56:32 +0530

    Navbar created

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/header/Header"
+import Navbar from "../../components/Navbar"
+import "./home.css"
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Header/>
+    </div>
+  )
+}
+
+export default Home
